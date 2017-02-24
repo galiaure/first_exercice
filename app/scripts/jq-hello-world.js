29c26a89391ea8ffe57fa8ab65721e470dd59f88
quentin grouleaud <quentin.grouleaud@gmail.com>
2017-02-24 20:33:18 +0100

    fin premier exercice, debut questionnaire JQuery

--- app/scripts/jq-hello-world.js
+++ app/scripts/jq-hello-world.js
@@ -10,9 +10,7 @@ $('[class~="liste"]').css('color','green');
 $('[class~="lettre"]').css('color','blue');
 $('[class$="e"]').css('font-size','18px');
 $('li:last').css('font-size','20px');
-$('p:even').css('color','red');
       }); 
-
 
 
 /* --------------- */
@@ -58,13 +56,48 @@ $('#animaux img').addClass('img_dimension');
 /* --------------- */
 
 
-/* connaitre les dimensions des bloc avec jquery */
+/* connaitre la position des bloc avec jquery */
 
 var posparent = $('#parent').position();
 var posenfant = $('#enfant').position();
 
+
 $('#resultat').text('Parent : x=' + posparent.left + ', y=' + posparent.top + ' Enfant : x=' + posenfant.left + ', y=' + posenfant.top);
                    
+/* --------------- */
 
+/* connaitre les dimensions des bloc avec jquery */
 
+var dimensions = 'width=' + $('div').width() + ', height=' + $('div').height() ;
+$('#resultat2').text(dimensions);
+
+/* --------------- */
+
+/* Associer des données des balises avec jquery */
+
+$(function() {
+    var div = $('div')[0];
+    $.data(div, 'mesValeurs', {premier: 'bonjour', deuxieme: 12, troisieme:'http://www.perdu.com' });
+    
+    var val1 = $.data(div, 'mesValeurs').premier;
+    var val2 = $.data(div, 'mesValeurs').deuxieme;
+    var val3 = $.data(div, 'mesValeurs').troisieme;    
+    $('#sp1').text(val1);
+    $('#sp2').text(val2);
+    $('#sp3').text(val3);
+});
+
+/* --------------- */
+
+/*  Insérer et remplacer des éléments dans le DOM   */
+
+$('h2').append('<hr>');
+$('h2').each(function(){
+    $(this).replaceWith('<h3>' + $(this).text() + '</h3>');
+});
+$('<hr>').prependTo($('h3:last'));
+$('#deux').after($('#un'));
+$('#deux').clone().insertBefore($('#un'));
+$('ul:last').clone().insertBefore($('h3:first'));
+$('ul:last').wrap('<strong></strong>');
 
