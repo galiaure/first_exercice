add532515bb07902a2e7cc5029c757adf739b3d3
galiaure <quentin.grouleaud@gmail.com>
2017-02-26 12:24:19 +0100

    création questionnaire avec JQuery

---
+++ app/scripts/questionnaire.js
@@ -0,0 +1,37 @@
+$('.reponse').hide();
+$('#reponse-global').hover(
+    
+  function(){
+    // Les instructions à exécuter au survol du lien
+    $('.reponse').show('slow');
+        /*  instruction question 1    */
+    if($('#r1').is(':checked')){
+        $('#reponse1').css('color','green');
+        $('#img1').attr('src','images/bon.png');
+      }else{
+          $('#reponse1').css('color','red');
+          $('#img1').attr('src','images/mauvais.png');
+            
+        }
+        /*  instruction question 2    */
+      if($('#r4').is(':checked')){
+                $('#reponse2').css('color','green');
+                $('#img2').attr('src','images/bon.png');
+            }else{
+                $('#reponse2').css('color','red');
+                $('#img2').attr('src','images/mauvais.png');
+            }
+        /*  instruction question 3    */
+      if($('#r8').is(':checked')){
+                    $('#reponse3').css('color','green');
+                    $('#img3').attr('src','images/bon.png');
+                } else{
+                    $('#reponse3').css('color','red');   $('#img3').attr('src','images/mauvais.png');
+                }
+  },
+  function() {
+    // Les instructions à exécuter lorsque le lien n'est plus survolé
+    $('.reponse').hide('slow');
+    $('.question img').attr('src','images/question.png');  
+  }
+);
