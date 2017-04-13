7cb5ea89352e562602cceada0ffc3b055117a0c9
quentin grouleaud <quentin.grouleaud@gmail.com>
2017-04-13 14:36:03 +0200

    save

--- app/scripts/evenementielle_avancer.js
+++ app/scripts/evenementielle_avancer.js
@@ -1,13 +1,31 @@
 // Jeu au clic
 
  $(function() {
+     
     function traitement() {
       alert('Image cliquée');
-    };  
+    }; 
+     
     $('#activer').on('click', function() {
-      $('#iron-man').click(traitement);
+      $('#iron_man').click(traitement);
     });
+     
     $('#desactiver').on('click', function() {
       $('#iron_man').off('click', traitement);
     });
+     
 });
+
+//  Evenement unique et déclenchement d'évènement
+
+$(function() {
+    $('#jurassik').click(function(event,texte) {
+      if (texte == undefined)
+        texte = "par vous";
+      $('#message').text('L\'image a été cliquée ' + texte).fadeIn(1000).fadeOut(1000);
+    });
+    $('button').click(function() {
+      $('#jurassik').trigger('click', 'par jQuery');
+    });
+  });
+    
